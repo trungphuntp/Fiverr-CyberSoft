5fcbb0e40c1755d11057328801b5be6b952e4d9e
trungphuntp <trungphuntp@gmail.com>
2025-03-07 01:49:22 +0700

    fix admin

--- src/app/(pages)/admin/layout.jsx
+++ src/app/(pages)/admin/layout.jsx
@@ -3,19 +3,28 @@ import ReduxProvider from "@/app/components/ReduxProvider/page";
 import PATH from "@/app/constants/path";
 import { handleSetMessage } from "@/app/store/reducers/messageReducer";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const LayoutAdmin = ({ children }) => {
     const { profile } = useSelector((state) => state.profile);
     const dispatch = useDispatch();
     const router = useRouter();
-    if (!!profile?.role !== "ADMIN") {
-        router.push(PATH.HOME);
-        dispatch(
-            handleSetMessage(["You do not have permission to access the admin page", "error"])
-        );
-    }
+
+    useEffect(() => {
+        if (!!profile?.id) {
+            if (!!profile?.role !== "ADMIN") {
+                router.push(PATH.HOME);
+                dispatch(
+                    handleSetMessage([
+                        "You do not have permission to access the admin page",
+                        "error",
+                    ])
+                );
+            }
+        }
+    }, [profile]);
+
     return <ReduxProvider>{children}</ReduxProvider>;
 };
 

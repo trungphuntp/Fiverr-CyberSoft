c8a98354e7d2a179bc1af248c44ce7637e6f74da
trungphuntp <trungphuntp@gmail.com>
2025-03-07 02:50:43 +0700

    test vercel middleware 3

--- src/middleware.js
+++ src/middleware.js
@@ -3,18 +3,17 @@ import { NextResponse } from "next/server";
 export const middleware = async (request) => {
     const cookies = request.cookies;
     const urlPathname = request.nextUrl.pathname;
-    const originPathname = request.nextUrl.origin;
 
     if (urlPathname === "/profile" || urlPathname === "/admin") {
         if (!cookies.has("tokenuser") && !cookies.has("iduser")) {
             return NextResponse.redirect(new URL("/login", request.url));
         }
     }
-    if (urlPathname === "/login" || urlPathname === "/register") {
-        if (cookies.has("tokenuser") && cookies.has("iduser")) {
-            return NextResponse.redirect(new URL("/", request.url));
-        }
-    }
+    // if (urlPathname === "/login" || urlPathname === "/register") {
+    //     if (cookies.has("tokenuser") && cookies.has("iduser")) {
+    //         return NextResponse.redirect(new URL("/", request.url));
+    //     }
+    // }
     return NextResponse.next();
 };
 
